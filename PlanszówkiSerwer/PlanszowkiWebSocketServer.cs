67090d98fc101896393d635429253b8c1d8bd866
MaciejWierz <maciej.wierzbicki97@gmail.com>
2019-12-17 19:19:38 +0100

    Creating project

---
+++ PlanszówkiSerwer/PlanszowkiWebSocketServer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SuperSocket.SocketBase;
+using SuperWebSocket;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanszówkiSerwer
+{
+    public class PlanszowkiWebSocketServer
+    {
+        private static WebSocketServer wsServer;
+
+
+        public PlanszowkiWebSocketServer()
+        {
+            wsServer = new WebSocketServer();
+            int port = 2137;
+            
+            wsServer.Setup(port);
+            wsServer.NewSessionConnected += WsServer_NewSesssionConnected;
+            wsServer.NewMessageReceived += WsServer_NewMessageReceived;
+            wsServer.NewDataReceived += WsServer_NewDataReceived;
+            wsServer.SessionClosed += WsServer_SessionClosed;
+            wsServer.Start();
+            while (true) ;
+           
+
+        }
+
+        private static void WsServer_SessionClosed(WebSocketSession session, CloseReason value)
+        {
+        }
+
+        private static void WsServer_NewDataReceived(WebSocketSession session, byte[] value)
+        {
+        }
+
+        private static void WsServer_NewMessageReceived(WebSocketSession session, string value)
+        {
+            session.Send("Tak jak Pan Jezus powiedział");
+        }
+
+        private static void WsServer_NewSesssionConnected(WebSocketSession session)
+        {
+        }
+
+        public void stopServer()
+        {
+            wsServer.Stop();
+        }
+    }
+}
---
+++ PlanszówkiSerwer/WebForm1.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PlanszówkiSerwer
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        PlanszowkiWebSocketServer server;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Thread thread = new Thread(serverRun);
+                thread.Start();
+            }
+
+        }
+
+
+        private void serverRun()
+        {
+            server = new PlanszowkiWebSocketServer();
+        }
+
+
+    }
+}
